e5ae8891bdf87f058b5d853be5c6a323b6b57567
Laura Torrents <lauratr89@gmail.com>
2022-12-20 18:56:17 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 
+        var searchFilter = () => {
+            let selectedOption = document.getElementById("inlineFormCustomSelect").value;
+            console.log(selectedOption);
+            const cards = document.getElementsByClassName("col");
+            console.log(cards[1]);
+            for (let i = 0; i < cards.length; i++) {
+               
+                if (cards[i].classList.contains(selectedOption) ) {
+                    cards[i].classList.remove("d-none");
+                } else {
+                    cards[i].classList.add("d-none");
+                }
+            }
+        }
