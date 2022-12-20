eb491aa836ae22d6f46a4def96259b1ffe22f074
Laura Torrents <lauratr89@gmail.com>
2022-12-20 23:56:11 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,47 @@
+//filter by city - and search by city//
 
-        var searchFilter = () => {
-            let selectedOption = document.getElementById("inlineFormCustomSelect").value;
-            console.log(selectedOption);
-            const cards = document.getElementsByClassName("col");
-            console.log(cards[1]);
-            for (let i = 0; i < cards.length; i++) {
-               
-                if (cards[i].classList.contains(selectedOption) ) {
-                    cards[i].classList.remove("d-none");
-                } else {
-                    cards[i].classList.add("d-none");
-                }
-            }
+var searchFilter = () => {
+    let selectedCity = document.getElementById("filterByCity").value;
+    console.log(selectedCity);
+    const input = document.querySelector(".form-control");
+    const cards = document.getElementsByClassName("col");
+    console.log(cards[1]);
+
+    let textBox = input.value;
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".card-body");
+        if ((cards[i].classList.contains(selectedCity) || selectedCity == "") && title.innerText.toLowerCase().indexOf(textBox.toLowerCase()) > -1) {
+            cards[i].classList.remove("d-none");
+        } else {
+            cards[i].classList.add("d-none");
         }
+    }
+}
+
+
+
+
+
+
+
+/* Maybe not working - filter 
+var searchFilter = () => {
+    let selectedCity = document.getElementById("filterByCity").value;
+    console.log(selectedCity);
+    const input = document.querySelector(".form-control");
+    const cards = document.getElementsByClassName("col");
+    console.log(cards[1]);
+​
+    let textBox = input.value;
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".card-body");
+        if ((cards[i].classList.contains(selectedCity) || selectedCity == "") && title.innerText.toLowerCase().indexOf(textBox.toLowerCase()) > -1) {
+            cards[i].classList.remove("d-none");
+        } else {
+            cards[i].classList.add("d-none");
+        }
+    }
+}
+*/
+
+
